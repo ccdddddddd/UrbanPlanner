1182a1b6eede03d94965d047c33298f467e807d0
张利帅 <zhanglishuai@tsingcloud.com>
2023-07-26 09:55:56 +0800

    V0.5.10 修改避让行人停车判断，行人距离主车安全距离1.2倍车宽改为1.6倍 修改标定量CalibrationVars.SpeedPlanAvoidPedestrian.a_max为1.5

--- SpeedPlanAvoidPedestrian.m
+++ SpeedPlanAvoidPedestrian.m
@@ -70,7 +70,7 @@ if dec_ped==1
             else
                 v_abs=max(0,-v_ped(i)*sign(l_ped(i))*cosd((psi_ped(i)+360)/2));
             end
-            d_ped=max([d_veh2ped(i)/max([speed 0.00001])*v_abs+0.5*w_veh*1.2 0]);                
+            d_ped=max([d_veh2ped(i)/max([speed 0.00001])*v_abs+0.5*w_veh*1.6 0]);                
             if abs(l_ped(i))<=d_ped
                 wait_ped=int16(1);
                 d_veh2stopline=d_veh2ped(i);
@@ -91,7 +91,7 @@ if wait_ped==1
             else
                 v_abs=max(0,-v_ped(i)*sign(l_ped(i))*cosd((psi_ped(i)+360)/2));
             end
-            timeGap=max([0 (abs(l_ped(i))-0.5*w_veh*1.2)/(eps+v_abs)]);
+            timeGap=max([0 (abs(l_ped(i))-0.5*w_veh*1.6)/(eps+v_abs)]);
             s_max=0.5*(min([speed+a_max*timeGap v_max_int])+speed)*timeGap;
             % if ~(d_veh2ped(i)<10 && s_max>d_veh2ped(i)) % 是否考虑车长？
             if ~(s_max>d_veh2ped(i)) % 是否考虑车长？
